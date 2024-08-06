f99301fa73c61e6e79b2257102088aa61a86954a
dialkq <dialkq@gmail.com>
2024-08-06 21:11:09 +0700

    pages/general

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ const Page = () => {
           type="submit"
           className="px-4 py-2 bg-blue-500 text-white rounded"
         >
-          Upload
+          Upload File
         </button>
       </form>
       {review && (
